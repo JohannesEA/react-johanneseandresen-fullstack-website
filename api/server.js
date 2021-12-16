3a3e6a7cc3afb270a55328032dc9e49bfa390846
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-16 23:45:42 +0100

    Add client link to api

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 
 
 const app = express();
@@ -7,15 +8,20 @@ const app = express();
 app.use(cors());
 
 
-app.get("/", (req, res) => {
-    res.send("Hello world!!");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hello world!!");
+// });
 
 
 app.get("/api/test", (req, res) => {
     res.send({ "name": "Johannes", "age": "22" });
 });
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 
 app.listen(process.env.PORT || 5000, () => {
