6ad004f23da7523ef4397c5efc4ba3492c694f7c
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-16 23:51:02 +0100

    test

--- api/server.js
+++ api/server.js
@@ -8,20 +8,20 @@ const app = express();
 app.use(cors());
 
 
-// app.get("/", (req, res) => {
-//     res.send("Hello world!!");
-// });
+app.get("/", (req, res) => {
+    res.send("Hello world!!");
+});
 
 
 app.get("/api/test", (req, res) => {
     res.send({ "name": "Johannes", "age": "22" });
 });
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
 
 
 app.listen(process.env.PORT || 5000, () => {
