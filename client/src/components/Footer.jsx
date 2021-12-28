9bc76ec0cbd3909be5bf739418cfce6969e0df26
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-28 20:42:58 +0100

    Fix footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,23 +1,52 @@
 import styled from "styled-components";
 import { Link } from "react-scroll";
-import {AiOutlineCopyrightCircle, AiOutlineGithub, AiOutlineInstagram} from "react-icons/ai"
+import {
+  AiOutlineCopyrightCircle,
+  AiOutlineGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+import getWindowDimensions from "../commonFunctions/Dimentions";
+
 const Footer = () => {
+  const { width } = getWindowDimensions();
   return (
     <Container>
       <Top>
-      <Right>
-            <SocialLink  onClick={() => (window.location.href = "https://github.com/JohannesEA")}><AiOutlineGithub fontSize={30}/></SocialLink>
-            <SocialLink onClick={() => (window.location.href = "https://www.instagram.com/johannesandresen/")}><AiOutlineInstagram fontSize={30}/></SocialLink>
+        <Right>
+          <SocialLink
+            onClick={() =>
+              (window.location.href = "https://github.com/JohannesEA")
+            }
+          >
+            <AiOutlineGithub fontSize={30} />
+          </SocialLink>
+          <SocialLink
+            onClick={() =>
+              (window.location.href =
+                "https://www.instagram.com/johannesandresen/")
+            }
+          >
+            <AiOutlineInstagram fontSize={30} />
+          </SocialLink>
         </Right>
-        <Left>
-          <Link to="hero" spy={true} smooth={true} offset={-100} duration={300}>
-            <Image src="/assets/logos/logo1.png" alt="footer-img" />{" "}
-          </Link>
-        </Left>
-   
+        {width > 800 && (
+          <Left>
+            <Link
+              to="hero"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={300}
+            >
+              <Image src="/assets/logos/logo1.png" alt="footer-img" />{" "}
+            </Link>
+          </Left>
+        )}
       </Top>
 
-      <Bottom>Copyright   <AiOutlineCopyrightCircle/>   johanneseandresen.no</Bottom>
+      <Bottom>
+        Copyright <AiOutlineCopyrightCircle /> johanneseandresen.no
+      </Bottom>
     </Container>
   );
 };
@@ -40,11 +69,7 @@ const Top = styled.div`
   width: 100%;
   height: 42vh;
 
-  @media (max-width: 800px) {
-    flex-direction: column;
-    height: 60vh;
 
-  }
 `;
 
 const Image = styled.img`
@@ -63,18 +88,21 @@ const Left = styled.div`
 
 const Right = styled.div`
   background-color: #032859;
-  width: 100%;
+  width: 50%;
   height: 100%;
   color: white;
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 0.8rem;
+
+  @media (max-width: 800px) {
+    justify-content: space-between;
+
+  }
 `;
 
-const SocialLink = styled.div`
-
-`;
+const SocialLink = styled.div``;
 const Bottom = styled.div`
   background-color: #0248a3;
   height: 5vh;
