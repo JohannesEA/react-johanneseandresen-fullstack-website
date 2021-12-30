b5248b57c8f3c6692d0b61584308ec4a2f8a4787
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-30 14:22:58 +0100

    Fix font family in textarea

--- client/src/pages/home/Contact.jsx
+++ client/src/pages/home/Contact.jsx
@@ -201,6 +201,11 @@ const TextArea = styled.textarea`
   max-width: 100%;
   font-size: 1.2rem;
   min-height: 10em;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
   @media (max-width: 400px){
     font-size: .8rem;
