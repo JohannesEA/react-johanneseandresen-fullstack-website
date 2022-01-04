b12040b450b537da12821a2457c54f6a5f365bdc
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2022-01-04 20:35:10 +0100

    Add crud operations to skills and projects in backend

---
+++ api/routes/skills.js
@@ -0,0 +1,59 @@
+const Skills = require("../models/skills");
+const router = require("express").Router();
+
+router.get("", async (req, res) => {
+  try {
+    const skills = await Skills.find();
+    res.status(200).json(skills);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const skills = await Skills.findById(id);
+    res.status(200).json(skills);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedSkill = await Skills.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedSkill);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const skill = new Skills(req.body);
+
+  try {
+    const savedSkill = await skill.save();
+    res.status(200).json(savedSkill);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const deleteSkill = await Skills.findByIdAndDelete(req.params.id);
+
+    res.status(200).json(deleteSkill);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const skillsRoute = require("./routes/skills");
+const projectRoute = require("./routes/project");
+
 // const path = require('path');
-
-
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect(
+    "mongodb+srv://johannesea:BraHAnDek527!Jek!@cluster0.0nfuw.mongodb.net/johannesea_website?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("db connection successfull"))
+  .catch((err) => console.log(err));
 
 app.use(cors());
-
+app.use(express.json());
+app.use("/api/skills", skillsRoute);
+app.use("/api/projects", projectRoute);
 
 app.get("/", (req, res) => {
-    res.send("Hello world!!");
+  res.send("Server is running..");
 });
-
-
-app.get("/api/test", (req, res) => {
-    res.send({ "name": "Johannes", "age": "22" });
-});
-
-// app.use(express.static(path.join(__dirname, '../client/build')));
-
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// });
-
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Server is running..");
+  console.log("Server is running..");
 });
-
